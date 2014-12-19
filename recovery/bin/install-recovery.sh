008a41301ac2caeca9a27800a2b7cf90ba96f6a7
Safarend <s.a.f.a.r.end@gmail.com>
2014-12-20 02:03:55 +1300

    first

---
+++ recovery/bin/install-recovery.sh
@@ -0,0 +1,42 @@
+#!/system/bin/sh
+if ! applypatch -c EMMC:/dev/block/mmcblk0p10:8044816:38462b6e0646194e2b44e683e066cf58127ea1bf; then
+  applypatch EMMC:/dev/block/mmcblk0p9:7614736:c4041718628b426067fc4877085cf50e11966600 EMMC:/dev/block/mmcblk0p10 38462b6e0646194e2b44e683e066cf58127ea1bf 8044816 c4041718628b426067fc4877085cf50e11966600:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+else
+  log -t recovery "Recovery image already installed"
+fi
+
+if [ -e /cache/recovery/command ] ; then
+  PACKAGE_PATH=""
+  SEARCH_COMMAND="--update_package"
+  PATH_POS=16
+  if [ -e '/system/bin/grep' ] ; then
+    PACKAGE_PATH=`cat /cache/recovery/command | grep 'update_package'`
+    PACKAGE_ORG_PATH=`cat /cache/recovery/command | grep 'update_org_package'`
+    if [ "$PACKAGE_ORG_PATH" != "" ] ; then
+      PACKAGE_PATH=$PACKAGE_ORG_PATH
+      SEARCH_COMMAND="--update_org_package"
+      PATH_POS=20
+    fi
+    if [ -e /cache/recovery/saved" ] ; then
+      rm -rf /cache/recovery/saved
+    fi
+
+    if [ -e /data/.recovery/saved" ] ; then
+      rm -rf /data/.recovery/saved
+    fi
+  fi
+  if [ "$PACKAGE_PATH" != "" ] ; then
+    for PACKAGE_LINE in $PACKAGE_PATH
+    do
+      if [ ${PACKAGE_LINE:0:$PATH_POS} == $SEARCH_COMMAND ] ; then
+        break
+      fi
+    done
+    let PATH_POS+=1
+    PACKAGE_PATH=${PACKAGE_LINE:$PATH_POS}
+    if [ "$PACKAGE_PATH" != "" ] ; then
+      rm $PACKAGE_PATH
+    fi
+  fi
+  rm /cache/recovery/command
+fi
